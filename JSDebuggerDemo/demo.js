13a70848cad5ec3d53f7d143fefec1efcbbeb41a
SatanWoo <applewuziqi@gmail.com>
2018-10-05 02:16:54 +0800

    add more complicated test case

--- JSDebuggerDemo/demo.js
+++ JSDebuggerDemo/demo.js
@@ -1,9 +1,9 @@
 //ViewController.test_(60, 6, 7);
 
-//var array = NSArray.arrayWithObjects_(72, 2, 3);
+var array = NSArray.arrayWithObjects_(72, 2, 3);
 
-//ViewController.printNSArray_(array);
+ViewController.printNSArray_(array);
 ViewController.testStr_value_("wuziqi niubi", 1.0, 2.0, 7.0);
 
-// var label = choose(UILabel)[0];
-// label.text = "SatanWoo";
+var label = choose(UILabel)[0];
+label.text = "SatanWoo";
