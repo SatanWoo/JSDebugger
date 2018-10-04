71b2b11928872c8542a192c34f8cc02c67722427
SatanWoo <applewuziqi@gmail.com>
2018-10-05 02:05:24 +0800

    1. Add Variadic Argument Demo 2. Fix Demo Type Error.

--- JSDebuggerDemo/JSDebuggerDemo/ViewController.m
+++ JSDebuggerDemo/JSDebuggerDemo/ViewController.m
@@ -24,33 +24,9 @@
 {
     [super viewDidLoad];
     
-    [ViewController test:7, 8, 9, nil];
+    //[ViewController test:7, 8, 9, nil];
     
     [[JDEngine engine] start];
-    
-    /*
-    
-    NSNumber *number = [NSNumber numberWithInt:5];
-    NSNumber *number2 = [NSNumber numberWithInt:20];
-    NSNumber *number3 = [NSNumber numberWithInt:30];
-    
-    *(void **)address = (__bridge void *)number;
-    *(void **)(address + sizeof(void *)) = (__bridge void *)number2;
-    *(void **)(address + 2 * sizeof(void *)) = (__bridge void *)number3;
-    
-    [self test:(__bridge NSNumber *)(address), nil];*/
-    
-    
-    void *address = malloc(sizeof(int) * 3);
-    
-    *(int *)address = 50;
-    *(int *)(address + sizeof(int)) = 10;
-    *(int *)(address + 2 * sizeof(int)) = 20;
-    
-    //[self printAddress:address];
-    
-    free(address);
-    //NSArray *arr = [NSArray arrayWithObjects:@(1), @(2), @(3), nil];
     
     dispatch_async(dispatch_get_main_queue(), ^{
         [[JDEngine engine] evaluateScriptAtPath:[[NSBundle mainBundle] pathForResource:@"demo" ofType:@"js"]];
@@ -77,17 +53,6 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)printAddress:(void *)address
-{
-    id n1 = (__bridge id)(*(void**)address);
-    id n2 = (__bridge id)(*(void**)(address + sizeof(void *)));
-    id n3 = (__bridge id)(*(void**)(address + 2 * sizeof(void *)));
-    
-    NSLog(@"number 1 is %@", n1);
-                          
-    NSLog(@"number 2 is %@", n2);
-    NSLog(@"number 3 is %@", n3);
-}
 
 + (void)test:(int)first,...NS_REQUIRES_NIL_TERMINATION
 {
@@ -108,27 +73,32 @@
     va_end(list);
 }
 
-/*- (void)test:(NSNumber *)first,...NS_REQUIRES_NIL_TERMINATION
++ (void)printNSArray:(NSArray *)array
 {
+    NSLog(@"array is %@", array);
+}
+
++ (void)testStr:(NSString *)str value:(double)d,...NS_REQUIRES_NIL_TERMINATION
+{
+    NSLog(@"str is %@", str);
+    
     va_list list;
     
     //遍历开始
     
-    va_start(list, first);
+    va_start(list, d);
     
-    NSLog(@"base address is %p", &first);
+    NSLog(@"double start is %g", d);
     
-    //知道读取到下一个时nil时结束递增
+    double arg;
     
-    for (NSNumber *str = first; str != nil; str = va_arg(list, NSNumber*)) {
-        
-        NSLog(@"%@",str);
-        
+    while ((arg = va_arg(list, double))) {
+        NSLog(@"当前参数:%g 地址:%p" , arg, &arg);
     }
     
-    //结束遍历
-    
     va_end(list);
-}*/
+    
+    
+}
 
 @end
--- JSDebuggerDemo/demo.js
+++ JSDebuggerDemo/demo.js
@@ -1,21 +1,9 @@
-//var vcs = choose(ViewController);
-//var vc = vcs[0];
-
-// vc.view().setBackgroundColor_(UIColor.s());
-
-ViewController.test_(60, 6, 7);
+//ViewController.test_(60, 6, 7);
 
 //var array = NSArray.arrayWithObjects_(72, 2, 3);
 
+//ViewController.printNSArray_(array);
+ViewController.testStr_value_("wuziqi niubi", 1.0, 2.0, 7.0);
+
 // var label = choose(UILabel)[0];
 // label.text = "SatanWoo";
-
-// var arr = [1, 2, 3];
-
-// var k = NSMutableArray.alloc().init();
-
-// k.addObject_(5);
-
-// vc.printNSArray_(k);
-
-//vc.printNSArray_(array);
